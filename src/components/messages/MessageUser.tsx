96793d04db7c83068faf17f7f190ce205e9a0855
kreid01 <kieranreid2014@icloud.com>
2022-11-15 18:04:19 +0000

    Fixed Messaging

--- src/components/messages/MessageUser.tsx
+++ src/components/messages/MessageUser.tsx
@@ -8,7 +8,7 @@ import { sendMessage } from "../../services/messages/sendMessage";
 import { connection } from "../../services/messages/sendMessage";
 
 import axios from "axios";
-import { useInfiniteQuery } from "react-query";
+import { InfiniteData, useInfiniteQuery } from "react-query";
 
 interface Props {
   user: IUser;
@@ -33,7 +33,7 @@ const getMesages = async (queryKey: any, pageParam: number) => {
       },
     }
   );
-  return data;
+  return data as Message[];
 };
 
 export const MessageUser: React.FC<Props> = ({ user }) => {
@@ -54,13 +54,18 @@ export const MessageUser: React.FC<Props> = ({ user }) => {
   } = useInfiniteQuery({
     queryKey: ["posts", chat?.id],
     queryFn: ({ pageParam = 1 }) => getMesages(chat?.id, pageParam),
-    getNextPageParam: (lastPage, allPages) => {
-      const nextPage = allPages.length + 1;
+    select: (data) => ({
+      pages: [...data.pages].reverse(),
+      pageParams: [...data.pageParams],
+    }),
+    getNextPageParam: (lastPage: Message[], allPages) => {
+      const nextPage: number = allPages.length + 1;
       return lastPage.length !== 0 ? nextPage : undefined;
     },
   });
   const [isMessaging, setIsMessaging] = useState(false);
   const [currentMessanger, setCurrentMessager] = useState<IUser>();
+  const [newMessages, setNewMessages] = useState<newMessage[]>([]);
   const startMessage = (user: IUser) => {
     setIsMessaging(true);
     setCurrentMessager(user as IUser);
@@ -71,6 +76,7 @@ export const MessageUser: React.FC<Props> = ({ user }) => {
   };
 
   const element = document.getElementById("messages");
+
   const sendMessageToUser = () => {
     const newMessage = {
       senderName: user?.userName as string,
@@ -94,7 +100,11 @@ export const MessageUser: React.FC<Props> = ({ user }) => {
 
   useEffect(() => {
     connection.on("RecieveNewMessage", (newMessage) => {
-      if (chat?.id === newMessage.chatId) refetch();
+      if (
+        chat?.id === newMessage.chatId &&
+        currentUser?.userName !== newMessage.senderName
+      )
+        refetch();
     });
   }, []);
 
@@ -122,7 +132,14 @@ export const MessageUser: React.FC<Props> = ({ user }) => {
     senderName: string;
     messageContent: string;
   };
-  const [newMessages, setNewMessages] = useState<newMessage[]>([]);
+
+  useEffect(() => {
+    if (element != null) element.scrollTop = element.scrollHeight;
+  }, [data]);
+
+  useEffect(() => {
+    if (element != null) element.scrollTop = element.scrollHeight;
+  }, [newMessages]);
 
   return (
     <div>
@@ -150,25 +167,47 @@ export const MessageUser: React.FC<Props> = ({ user }) => {
           {data &&
             data?.pages.map((page) =>
               page.map((message, i) => {
-                return (
-                  <div className="my-4 -ml-20 border-b-2 flex flex-col">
-                    <p className="text-sm text-center text-gray-300 ">
-                      {message.created.substring(0, 10)}
-                    </p>
-                    {message.senderName !== user?.userName ? (
-                      <p className="bg-blue-500 text-white self-end mr-3 px-4 py-2 rounded-2xl mb-2">
-                        {message.messageContent}
-                      </p>
-                    ) : (
+                if ((i = page.length - 1)) {
+                  return (
+                    <div className="my-4 -ml-20 border-b-2 flex flex-col">
                       <p
                         ref={loader}
-                        className="bg-gray-600 text-white self-start px-4 py-2 rounded-2xl mb-2"
+                        className="text-sm text-center text-gray-300 "
                       >
-                        {message.messageContent}
+                        {message.created.substring(0, 10)}
                       </p>
-                    )}
-                  </div>
-                );
+                      {message.senderName !== user?.userName ? (
+                        <p className="bg-blue-500 text-white self-end mr-3 px-4 py-2 rounded-2xl mb-2">
+                          {message.messageContent}
+                        </p>
+                      ) : (
+                        <p
+                          ref={loader}
+                          className="bg-gray-600 text-white self-start px-4 py-2 rounded-2xl mb-2"
+                        >
+                          {message.messageContent}
+                        </p>
+                      )}
+                    </div>
+                  );
+                } else {
+                  return (
+                    <div className="my-4 -ml-20 border-b-2 flex flex-col">
+                      <p className="text-sm text-center text-gray-300 ">
+                        {message.created.substring(0, 10)}
+                      </p>
+                      {message.senderName !== user?.userName ? (
+                        <p className="bg-blue-500 text-white self-end mr-3 px-4 py-2 rounded-2xl mb-2">
+                          {message.messageContent}
+                        </p>
+                      ) : (
+                        <p className="bg-gray-600 text-white self-start px-4 py-2 rounded-2xl mb-2">
+                          {message.messageContent}
+                        </p>
+                      )}
+                    </div>
+                  );
+                }
               })
             )}
           {newMessages &&
